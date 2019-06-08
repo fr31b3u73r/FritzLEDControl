60de5abae9c9df3c6f87867c1be308114a90eecd
fr31b3u73r <fr31b3u73r@gmail.com>
2019-06-08 10:52:42 +0200

    Implementation, updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FritzLEDControl
+[![Python](https://img.shields.io/badge/python-3-brightgreen.svg)]() [![Donate](https://img.shields.io/badge/donate-PayPal-brightgreen.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=5654A67GA3GHA)
+
 Turn your FRITZ!Box LEDs on and off via Python scripts (only works for FRITZ!OS 7.10 and newer)
+
+## Setup
+Install Python 3 and required libraries then modify configuration on top of script (URL + password).
+After that you can call the scripts to turn on/off LEDs of your FRITZ!Box. Also usable via cronjob e.g. to turn off LEDs during night.
---
+++ led_on.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+from urllib import request, parse
+import hashlib
+
+# configuration
+FRITZBOX_PASSWORD = ''
+FRITZBOX_BASE_URL = ''
+
+def get_sid():
+	fb = request.urlopen(FRITZBOX_BASE_URL + 'login_sid.lua')
+	dom = ET.parse(fb)
+	sid = dom.findtext('./SID')
+	challenge = dom.findtext('Challenge')
+	if sid == '0000000000000000':
+		md5 = hashlib.md5()
+		md5.update(challenge.encode('utf-16le'))
+		md5.update('-'.encode('utf-16le'))
+		md5.update(FRITZBOX_PASSWORD.encode('utf-16le'))
+		response = challenge + '-' + md5.hexdigest()
+		url = FRITZBOX_BASE_URL + 'login_sid.lua?&response=' + response
+		fb = request.urlopen(url)
+		dom = ET.parse(fb)
+		sid = dom.findtext('./SID')
+
+	if sid == '0000000000000000':
+		raise PermissionError('access denied')
+
+	return sid
+
+sid = get_sid()
+
+datadict = {'xhr' :	1,
+	'led_display': 0,
+	'apply': '',
+	'sid':	sid,
+	'lang':	'de',
+	'page':	'led'}
+
+data = parse.urlencode(datadict).encode()
+
+req =  request.Request(FRITZBOX_BASE_URL + "data.lua", data=data)
+resp = request.urlopen(req)
