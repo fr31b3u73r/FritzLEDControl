9510359a6e6929854553e5ac23e62f82ddecbbc5
fr31b3u73r <fr31b3u73r@gmail.com>
2019-06-08 17:14:11 +0200

    Added code comments and logout

--- led_off.py
+++ led_off.py
@@ -9,6 +9,7 @@ FRITZBOX_PASSWORD = ''
 FRITZBOX_USER = ''
 FRITZBOX_BASE_URL = ''
 
+# method to retrieve SID
 def get_sid():
 	fb = request.urlopen(FRITZBOX_BASE_URL + 'login_sid.lua')
 	dom = ET.parse(fb)
@@ -33,8 +34,10 @@ def get_sid():
 
 	return sid
 
+# get a valid SID
 sid = get_sid()
 
+# HTTP Post request body content
 datadict = {'xhr' :	1,
 	'led_display': 2,
 	'apply': '',
@@ -45,4 +48,8 @@ datadict = {'xhr' :	1,
 data = parse.urlencode(datadict).encode()
 
 req =  request.Request(FRITZBOX_BASE_URL + "data.lua", data=data)
+# execute request to switch LEDs
 resp = request.urlopen(req)
+
+# log out
+request.urlopen(FRITZBOX_BASE_URL + "home/home.lua?logout=1&sid=" + sid)
